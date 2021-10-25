083aa3851973ff3c27694455108356b87ee7990a
Babar Baig <BobBaig@outlook.com>
2021-10-24 21:10:25 -0400

    Remove dead code

--- boston-200k+-earners.js
+++ boston-200k+-earners.js
@@ -9,29 +9,6 @@ function buildTop5(person){
   top5.sort((person1, person2) => parseInt(person2[11]) - parseInt(person1[11]))
 }
 
-function renderPosts2(boston, container) {
-  const people = boston.data;
-  const len = boston.data.length;
-
-  for (let i = 0; i < len; i++){
-    buildTop5(people[i])
-  }
-
-  let html = '';
-  // for (let i = 0; i < len; i++) {
-  //   html += '<li class="post">' + '<h2>' + people[i][8] + '</h2>'
-  //      + '<h3>' + people[i][11] + '</h3>';
-  // }
-  for (let i = 0; i < 5; i++) {
-    html += '<li class="topSalaries">' + '<h2>' + top5[i][8] + '</h2>'
-        + '<h3>' + top5[i][11] + '</h3>';
-  }
-
-  // TODO: add code to display the html variable inside a ul element with id="data"
-  // Hint: you can use the container parameter's innerHTML property to insert Html tags
-  container.innerHTML = '<ul id = "data">' + html + '</ul>';
-}
-
 function renderPosts(boston, container) {
   const people = boston.data;
   // const len = boston.data.length;
